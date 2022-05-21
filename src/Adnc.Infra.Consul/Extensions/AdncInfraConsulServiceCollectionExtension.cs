583bfc29826af0e1287975193b7c50f2995cdfdf
yanh1993 <891367701@qq.com>
2022-05-21 15:26:21 +0800

    'up'

--- src/Adnc.Infra.Consul/Extensions/AdncInfraConsulServiceCollectionExtension.cs
+++ src/Adnc.Infra.Consul/Extensions/AdncInfraConsulServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
-﻿using Adnc.Infra.Core.Adnc.Configuration;
+﻿using Adnc.Infra.Consul.Consumer;
+using Adnc.Infra.Consul.TokenGenerator;
+using Adnc.Infra.Core.Adnc.Configuration;
 using Consul;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,9 +19,9 @@ namespace Adnc.Infra.Consul.Extensions
 
         public static IServiceCollection AddAdncInfraConsul(this IServiceCollection services, ConsulConfig consulConfig)
         {
-            //services.AddScoped<ITokenGenerator, DefaultTokenGenerator>();
-            //services.AddScoped<SimpleDiscoveryDelegatingHandler>();
-            //services.AddScoped<ConsulDiscoverDelegatingHandler>();
+            services.AddScoped<ITokenGenerator, DefaultTokenGenerator>();
+            services.AddScoped<SimpleDiscoveryDelegatingHandler>();
+            services.AddScoped<ConsulDiscoverDelegatingHandler>();
             services.AddSingleton(x => new ConsulClient(x => x.Address = new Uri(consulConfig.ConsulUrl)));
             return services;
         }
---
+++ src/Adnc.Infra.Consul/TokenGenerator/ITokenGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adnc.Infra.Consul.TokenGenerator
+{
+    public interface ITokenGenerator
+    {
+        public string Scheme { get; }
+
+        /// <summary>
+        /// 创建一个token
+        /// </summary>
+        /// <returns></returns>
+        public string Create();
+    }
+}
