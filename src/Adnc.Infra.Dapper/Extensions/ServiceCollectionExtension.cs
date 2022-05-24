8edd7b834c2c021821db72417756c49cbb42c4e8
yanh1993 <891367701@qq.com>
2022-05-25 00:37:40 +0800

    'up'

---
+++ src/Adnc.Infra.Dapper/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Adnc.Infra.Dapper.Repositories;
+using Adnc.Infra.Repository.IRepositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adnc.Infra.Dapper.Extensions
+{
+    public static class AdncInfraDapperServiceCollectionExtension
+    {
+        public static IServiceCollection AddAdncInfraDapper(this IServiceCollection services)
+        {
+            services.TryAddScoped<IAdoExecuterWithQuerierRepository, DapperRepository>();
+            services.TryAddScoped<IAdoExecuterRepository, DapperRepository>();
+            services.TryAddScoped<IAdoQuerierRepository, DapperRepository>();
+            return services;
+        }
+    }
+}
