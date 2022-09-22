d56926d65e728d4bf0a65428a2c673b55435f587
DESKTOP-E91V2B7\win <891367701@qq.com>
2022-09-22 17:16:43 +0800

    'up'

--- src/Adnc.Infra.EventBus/RabbitMq/RabbitMqConnection.cs
+++ src/Adnc.Infra.EventBus/RabbitMq/RabbitMqConnection.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Adnc.Infra.Core.Adnc.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Adnc.Infra.EventBus.RabbitMq
 {
-    internal class RabbitMqConnection
+    public interface IRabbitMqConnection
     {
+        IConnection Connection { get; }
+    }
+
+    internal sealed class RabbitMqConnection : IRabbitMqConnection
+    {
+        private static volatile RabbitMqConnection? _uniqueInstance;
+        private static readonly object _lockObject = new();
+        private ILogger<dynamic> _logger = default!;
+        public IConnection Connection { get; private set; } = default!;
+
+        private RabbitMqConnection()
+        {
+
+        }
+
+        internal static RabbitMqConnection GetInstance(IOptions<RabbitMqConfig> options, string clientProvidedName, ILogger<dynamic> logger)
+        {
+            if (_uniqueInstance is null)
+            {
+                lock (_lockObject)
+                {
+                    if (_uniqueInstance is null)
+                    {
+                        _uniqueInstance = new RabbitMqConnection(options, clientProvidedName, logger);
+                    }
+                }
+            }
+            return _uniqueInstance;
+        }
+
+        private RabbitMqConnection(IOptions<RabbitMqConfig> options, string clientProvidedName, ILogger<dynamic> logger)
+        {
+            _logger = logger;
+
+            var factory = new ConnectionFactory()
+            {
+                ClientProvidedName = clientProvidedName,
+                HostName = options.Value.HostName,
+                VirtualHost = options.Value.VirtualHost,
+                UserName = options.Value.UserName,
+                Password = options.Value.Password,
+                Port = options.Value.Port,
+                //Rabbitmq集群必需加这两个参数
+                AutomaticRecoveryEnabled = true,
+                //TopologyRecoveryEnabled=true
+            };
+
+            Policy.Handle<SocketException>()
+                  .Or<BrokerUnreachableException>()
+                  .WaitAndRetry(2, retryAttempt => TimeSpan.FromSeconds(1), (ex, time, retryCount, content) =>
+                  {
+                      if (2 == retryCount)
+                          throw ex;
+                      _logger.LogError(ex, string.Format("{0}:{1}", retryCount, ex.Message));
+                  })
+                  .Execute(() =>
+                  {
+                      Connection = factory.CreateConnection();
+                  });
+        }
     }
 }
--- src/Adnc.Infra.EventBus/RabbitMq/RabbitMqProducer.cs
+++ src/Adnc.Infra.EventBus/RabbitMq/RabbitMqProducer.cs
@@ -1,12 +1,102 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Adnc.Infra.EventBus.RabbitMq
 {
-    internal class RabbitMqProducer
+    public class RabbitMqProducer : IDisposable
     {
+        private readonly IModel _channel;
+        private readonly ILogger<RabbitMqProducer> _logger;
+
+        public RabbitMqProducer(IRabbitMqConnection rabbitMqConnection, ILogger<RabbitMqProducer> logger)
+        {
+            _logger = logger;
+            _channel = rabbitMqConnection.Connection.CreateModel();
+        }
+
+        /// <summary>
+        /// 简单队列,不通过交换机
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="queueName"></param>
+        /// <param name="message"></param>
+        //public virtual void BasicPublish<TMessage>(string queueName, TMessage message)
+        //{
+        //    _logger.LogInformation($"PushMessage,queueName:{queueName}");
+
+        //    _channel.QueueDeclare(queue: queueName,
+        //                          durable: false,
+        //                          exclusive: false,
+        //                          autoDelete: false,
+        //                          arguments: null);
+
+        //    var content = JsonSerializer.Serialize(message);
+        //    var body = Encoding.UTF8.GetBytes(content);
+
+        //    _channel.BasicPublish(exchange: string.Empty,
+        //                          routingKey: queueName,
+        //                          basicProperties: null,
+        //                          body: body);
+        //}
+
+        public virtual void BasicPublish<TMessage>(string exchange
+            , string routingKey
+            , TMessage message
+            , IBasicProperties? properties = null
+            , bool mandatory = false
+        )
+        {
+            Policy.Handle<Exception>()
+                  .WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(1), (ex, time, retryCount, content) =>
+                  {
+                      _logger.LogError(ex, string.Format("{0}:{1}", retryCount, ex.Message));
+                  })
+                  .Execute(() =>
+                  {
+                      var content = message as string;
+                      if (content == null)
+                          content = JsonSerializer.Serialize(message);
+
+                      var body = Encoding.UTF8.GetBytes(content);
+
+                      //当mandatory标志位设置为true时，如果exchange根据自身类型和消息routingKey无法找到一个合适的queue存储消息
+                      //那么broker会调用basic.return方法将消息返还给生产者;
+                      //当mandatory设置为false时，出现上述情况broker会直接将消息丢弃
+
+                      _channel.BasicPublish(exchange, routingKey, mandatory, basicProperties: properties, body);
+
+                      //开启发布消息确认模式
+                      //_channel.ConfirmSelect();
+                      //消息是否到达服务器
+                      //bool publishStatus = _channel.WaitForConfirms();
+                  });
+        }
+
+        public virtual IBasicProperties CreateBasicProperties()
+        {
+            return _channel.CreateBasicProperties();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_channel != null)
+                    _channel.Dispose();
+            }
+        }
     }
 }
